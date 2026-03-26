1a7eb1861111bb44721258d597f1e95f349046ad
Serginho <serginhoagostinho@gmail.com>
2026-03-26 15:30:28 -0300

    Implementado otimizção em trajetória

--- js/global/flipbook.js
+++ js/global/flipbook.js
@@ -1,122 +1,156 @@
-function criarLivro() {
-  const nPag = 100;
-
-  for (let i = 1; i <= nPag; i++) {
-
-    const paper = document.createElement('div');
-    i === 1 ? paper.setAttribute('class', 'paper active') : paper.setAttribute('class', 'paper');
-    paper.style.zIndex = nPag + 1 - i;
-    const front = document.createElement('div');
-    front.setAttribute('class', 'front');
-    const back = document.createElement('div');
-    back.setAttribute('class', 'back');
-    const img1 = document.createElement('img');
-    img1.setAttribute('src', 'assets/images/pages/porto--trajetoria/pagina ' + (i * 2 - 1) + '.webp');
-    const img2 = document.createElement('img');
-    img2.setAttribute('src', 'assets/images/pages/porto--trajetoria/pagina ' + (i * 2) + '.webp');
-
-    front.appendChild(img1);
-    back.appendChild(img2);
-    paper.appendChild(front);
-    paper.appendChild(back);
-    book.appendChild(paper);
-  }
-};
-criarLivro();
-
+const book = document.querySelector('.book');
 const papers = document.querySelectorAll('.paper');
-const prevBtn = document.querySelector('#prevBtn');
-const nextBtn = document.querySelector('#nextBtn');
 const pageInput = document.querySelector('#pageInput');
 const goBtn = document.querySelector('#goBtn');
+const nextBtn = document.querySelector('#nextBtn');
+const prevBtn = document.querySelector('#prevBtn');
 
-let currentState = 0; 
-const maxState = papers.length;
+let pag = -1;
 
-function updateBook() {
-  const isPortrait = window.innerHeight > window.innerWidth;
-
-  papers.forEach((paper, index) => {
-    if (index < currentState) {
-      // Páginas que já foram viradas
-      paper.classList.add('flipped');
-      setTimeout(() => {
-        paper.style.zIndex = index + 1;
-      }, 100);
-    } else {
-      // Páginas que ainda não foram viradas
-      paper.classList.remove('flipped');
-      setTimeout(() => {
-        paper.style.zIndex = papers.length - index;
-      }, 100);
-    }
-  });
-
-  // Ajuste de centralização (Apenas Landscape)
-  if (!isPortrait) {
-    book.style.transform = currentState > 0 ? "translateX(50%)" : "translateX(0%)";
-  } else {
-    book.style.transform = "translateX(0%)";
-  }
-}
-
-// Eventos dos Botões
-nextBtn.addEventListener("click", () => {
-  if (currentState < maxState) {
-    currentState++;
-    pageInput.value = currentState * 2 - 1;
-    updateBook();
-  }
+papers.forEach(p => {
+    p.addEventListener('click', () => {
+        passaPagina(p);
+    })
 });
 
-prevBtn.addEventListener("click", () => {
-  if (currentState > 0) {
-    currentState--;
-    pageInput.value = currentState * 2 - 1;
-    updateBook();
-  }
+goBtn.addEventListener('click', () => {
+    const n = Math.ceil((parseInt(pageInput.value) - pag) / 2);
+    for (let i = 1; i <= Math.abs(n); i++) {
+        let pag;
+        (n > 0) ? pag = document.querySelectorAll('.direita') : pag = document.querySelectorAll('.esquerda');
+        if (pag.length > 0) {
+            let max = pag[0];
+            pag.forEach(p => {
+                max.style.zIndex < p.style.zIndex ? max = p : max = max;
+            })
+            passaPagina(max);
+        }
+    }
 });
 
 pageInput.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    const targetPage = parseInt(pageInput.value);
-    // Como cada 'paper' tem 2 páginas, dividimos por 2 para achar o índice do paper
-    // Ex: Página 3 está no paper 2 (índice 1)
-    if (targetPage >= 0 && targetPage <= maxState * 2) {
-      currentState = Math.ceil(targetPage / 2);
-      updateBook();
-    } else {
-      alert("Página inválida");
+    if (e.key === "Enter") {
+        const n = Math.ceil((parseInt(pageInput.value) - pag) / 2);
+        for (let i = 1; i <= Math.abs(n); i++) {
+            let pag;
+            (n > 0) ? pag = document.querySelectorAll('.direita') : pag = document.querySelectorAll('.esquerda');
+            if (pag.length > 0) {
+                let max = pag[0];
+                pag.forEach(p => {
+                    max.style.zIndex < p.style.zIndex ? max = p : max = max;
+                })
+                passaPagina(max);
+            }
+        }
     }
-  }
-
 });
 
-// Evento do Input "Ir para página"
-goBtn.addEventListener("click", () => {
-  const targetPage = parseInt(pageInput.value);
-  // Como cada 'paper' tem 2 páginas, dividimos por 2 para achar o índice do paper
-  // Ex: Página 3 está no paper 2 (índice 1)
-  if (targetPage >= 0 && targetPage <= maxState * 2) {
-    currentState = Math.ceil(targetPage / 2);
-    updateBook();
-  } else {
-    alert("Página inválida");
-  }
-});
-
-// Mantém o clique direto na página funcionando
-papers.forEach((paper, index) => {
-  paper.addEventListener('click', () => {
-    if (paper.classList.contains('flipped')) {
-      currentState = index;
-    } else {
-      currentState = index + 1;
+nextBtn.addEventListener('click', () => {
+    const direita = document.querySelectorAll('.direita');
+    if (direita.length > 0) {
+        let max = direita[0];
+        direita.forEach(p => {
+            max.style.zIndex < p.style.zIndex ? max = p : max = max;
+        })
+        passaPagina(max);
     }
-    pageInput.value = currentState * 2 - 1;
-    updateBook();
-  });
 });
 
-// Inicializa o z-index correto
-updateBook();
+prevBtn.addEventListener('click', () => {
+    const esquerda = document.querySelectorAll('.esquerda');
+    if (esquerda.length > 0) {
+        let max = esquerda[0];
+        esquerda.forEach(p => {
+            max.style.zIndex < p.style.zIndex ? max = p : max = max;
+        })
+        passaPagina(max);
+    }
+});
+
+function passaPagina(p) {
+    let esquerda;
+    let direita;
+    papers.forEach(p => {
+        if (p.style.zIndex === '1') {
+            p.style.display = 'block';
+        }
+    })
+    document.querySelectorAll('.esquerda').forEach(e => {
+        if (e.style.zIndex === '5') {
+            e.style.zIndex = document.querySelectorAll('.esquerda').length;
+        }
+    })
+    document.querySelectorAll('.direita').forEach(e => {
+        if (e.style.zIndex === '5') {
+            e.style.zIndex = document.querySelectorAll('.direita').length;
+        }
+    })
+    if (p.getAttribute('class') === 'paper direita') {
+        p.setAttribute('class', 'paper esquerda');
+        esquerda = document.querySelectorAll('.esquerda');
+        direita = document.querySelectorAll('.direita');
+        p.style.zIndex = '5';
+        pag += 2;
+        cicloPagina(esquerda, direita);
+    }
+    else {
+        p.setAttribute('class', 'paper direita');
+        esquerda = document.querySelectorAll('.esquerda');
+        direita = document.querySelectorAll('.direita');
+
+        p.style.zIndex = '5';
+        pag -= 2;
+        cicloPagina(esquerda, direita);
+    }
+    moverLivro();
+    pageInput.value = pag;
+}
+
+function cicloPagina(e, d) {
+    if (d.length < 2 && pag < 197) {
+        d.forEach(i => {
+            i.style.zIndex++;
+        })
+        document.querySelectorAll('.esquerda').forEach(i => {
+            if (i.style.zIndex === '1') {
+                i.setAttribute('class', 'paper direita');
+                carregaPagina(i, 4);
+                i.style.display = 'none';
+            }
+            else if (i.style.zIndex !== '5') {
+                i.style.zIndex--;
+            }
+
+        })
+    }
+    else if (e.length < 2 && pag > 1) {
+        e.forEach(i => {
+            i.style.zIndex++;
+        })
+        document.querySelectorAll('.direita').forEach(i => {
+            if (i.style.zIndex === '1') {
+                i.setAttribute('class', 'paper esquerda');
+                carregaPagina(i, -2);
+                i.style.display = 'none';
+            }
+            else if (i.style.zIndex !== '5') {
+                i.style.zIndex--;
+            }
+        })
+    }
+}
+
+function carregaPagina(p, n) {
+    p.children[0].children[0].setAttribute('src', `assets/images/pages/porto--trajetoria/pagina ${pag + n}.webp`)
+    p.children[1].children[0].setAttribute('src', `assets/images/pages/porto--trajetoria/pagina ${pag + n + 1}.webp`)
+}
+
+function moverLivro() {
+    if (pag > -1) {
+        book.style.transform = 'translateX(270px)';
+    }
+    else {
+        book.style.transform = 'translateX(0)';
+    }
+}
+
